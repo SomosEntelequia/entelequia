b2cad5bd38c3daff2c9abdb50ac5dff616455fa8
StonesSolutions <131500823+StonesSolutions@users.noreply.github.com>
2026-03-05 17:45:00 -0600

    Update res_partner.py

--- stn_custom_entelequia/controllers/res_partner.py
+++ stn_custom_entelequia/controllers/res_partner.py
@@ -422,12 +422,14 @@ class ApiController(http.Controller):
             #        limit=1
             #   )            
             #   update_vals['l10n_mx_edi_payment_method_id'] = payment_term.id if payment_term else False
-           if contact_data.get('l10n_mx_edi_payment_method_id'):
-                sap_code = contact_data.get('l10n_mx_edi_payment_method_id')            
+           # Campos fiscales que son IDs
+            if contact_data.get('l10n_mx_edi_payment_method_id'):
+                sap_code = contact_data.get('l10n_mx_edi_payment_method_id')
+            
                 payment_term = request.env['account.payment.term'].sudo().search(
                     [('sap_payment_term_code', '=', sap_code)],
                     limit=1
-                )            
+                )        
                 if payment_term:
                     update_vals['property_payment_term_id'] = payment_term.id
             #fin
