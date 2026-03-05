d8fe4a2aa1ee8ed108a9e8bb6005051076d70e59
StonesSolutions <131500823+StonesSolutions@users.noreply.github.com>
2026-03-05 17:43:33 -0600

    Update res_partner.py

--- stn_custom_entelequia/controllers/res_partner.py
+++ stn_custom_entelequia/controllers/res_partner.py
@@ -423,7 +423,7 @@ class ApiController(http.Controller):
             #   )            
             #   update_vals['l10n_mx_edi_payment_method_id'] = payment_term.id if payment_term else False
            if contact_data.get('l10n_mx_edi_payment_method_id'):
-                sap_code = contact_data.get('property_payment_term_id')            
+                sap_code = contact_data.get('l10n_mx_edi_payment_method_id')            
                 payment_term = request.env['account.payment.term'].sudo().search(
                     [('sap_payment_term_code', '=', sap_code)],
                     limit=1
